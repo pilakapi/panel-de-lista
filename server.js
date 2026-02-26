3b17b11ea3c7f335be1916b8815540c69563df02
pilakapi <jonathansalin22@gmail.com>
2026-02-26 00:33:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+app.use(express.static(__dirname));
 
 // Crear tabla si no existe
 async function initDatabase() {
